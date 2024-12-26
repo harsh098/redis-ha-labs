6ec2ce0f058b5ef985d0c8a0a83838b36a301420
Harsh Mishra <hmisraji07@gmail.com>
2024-12-27 01:03:17 +0530

    Add ebs_csi_irsa_role to EKS Module

--- terraform/modules/eks/data.tf
+++ terraform/modules/eks/data.tf
@@ -1,3 +1,7 @@
 data "aws_availability_zones" "available" {}
 
 data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy" "ebs_csi_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -32,7 +32,7 @@ module "eks" {
     }
     aws-ebs-csi-driver = {
       most_recent = true
-      service_account_role_arn = var.iam_role_arn
+      service_account_role_arn = module.ebs_csi_irsa_role.iam_role_arn
     }
   }, var.cluster_addons)
 
@@ -111,6 +111,20 @@ module "vpc_cni_irsa" {
     main = {
       provider_arn               = module.eks.oidc_provider_arn
       namespace_service_accounts = ["kube-system:aws-node"]
+    }
+  }
+}
+
+module "ebs_csi_irsa_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name             = "ebs-csi"
+  attach_ebs_csi_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
     }
   }
 }
